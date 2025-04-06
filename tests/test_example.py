93a2f398a07d3e751284f4268ae8b4ed708dff52
acelya23 <acelya.caliskan2003@outlook.com>
2025-04-06 23:29:31 +0300

    Update test_example.py

--- tests/test_example.py
+++ tests/test_example.py
@@ -2,3 +2,5 @@ def test_addition():
     assert 1 + 1 == 2
 def test_addition():
     assert 1 + 1 == 3  # Yanlış assertion (1+1=2 olmalıydı)
+def test_addition():
+    assert 1 + 1 == 2  # Doğru assertion
